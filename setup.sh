37f8bb71e782a7e373f6c6bd0a389fc3160e9a5b
ArthurG <arthur.guo@uwaterloo.ca>
2019-03-30 18:01:19 -0700

    Add Vim stuff

---
+++ setup.sh
@@ -0,0 +1,10 @@
+# Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# NerdTree
+git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+cp .vimrc ~/.vimrc
+
+# copying git files
